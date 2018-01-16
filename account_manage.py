f734578512b93ee3d546d324c1a12cd77039a11a
GaryDoooo <gdu@gmx.us>
2018-01-15 23:44:46 -0600

    username letters and numbers only, figt module v1.

--- account_manage.py
+++ account_manage.py
@@ -1,126 +1,139 @@
 import pickle
-from vendingmachineworks import yes_or_no 
-import subprocess as sp
+from vendingmachineworks import yes_or_no
+# import subprocess as sp
 import os
 import getpass
-from terminaltables import SingleTable
+# from terminaltables import SingleTable
+import re
+
 
 class account:
-    def __init__(self,username,password,points=0,emoji_you_have=[0,0,0,0,0,0,0]):
-        self.username=username
-        self.password=password
-        self.points=points
-        self.emoji_you_have=emoji_you_have
-    
+    def __init__(self, username, password, points=0, emoji_you_have=[0, 0, 0, 0, 0, 0, 0]):
+        self.username = username
+        self.password = password
+        self.points = points
+        self.emoji_you_have = emoji_you_have
+
     def save(self):
-        pickle.dump(self,open("vending_accounts/"+self.username+".p","wb"),protocol=0)
-        
+        pickle.dump(self, open("vending_accounts/" +
+                               self.username + ".p", "wb"), protocol=0)
+
     def print_data(self):
-        print("username: "+self.username)
+        print("username: " + self.username)
         # print("password: ******")
         print("Having points: %d" % self.points)
         print(self.emoji_you_have[1:])
-    
+
     def del_account(self):
-        os.system("rm vending_accounts/"+self.username+".p")
-        
+        os.system("rm vending_accounts/" + self.username + ".p")
+
     def edit_data(self):
-        del_account=False
+        del_account = False
         while True:
             print("1. Change password.")
             print("2. Delete account.")
             print("3. Change points.")
             print("4. Save changes and exit.")
             print("5. Exit withou saving.")
-            action=int(input("Choose(1-3): "))
-            if action==1:
+            action = int(input("Choose(1-3): "))
+            if action == 1:
                 while True:
-                    password=getpass.getpass("Password: ")
-                    passwd2=getpass.getpass("Password again:")
-                    if passwd2==password:
+                    password = getpass.getpass("Password: ")
+                    passwd2 = getpass.getpass("Password again:")
+                    if passwd2 == password:
                         break
-                self.password=password
+                self.password = password
                 print("Got new password.")
-            elif action==2:
+            elif action == 2:
                 print("Account will be deleted if save changes.")
-                del_account==True
-            elif action==3:
-                new_points=int(input("New Points: "))
-                self.points=new_points
-            elif action==4:
+                del_account == True
+            elif action == 3:
+                new_points = int(input("New Points: "))
+                self.points = new_points
+            elif action == 4:
                 if del_account:
                     self.del_account()
                 else:
                     self.save()
                 break
-            elif action==5:
+            elif action == 5:
                 break
+
 
 def print_account_list():
     #  find vending_accounts/ -name "*.p" | wc -l
     print("Total account number:")
-    file_num=os.system('find vending_accounts/ -name "*.p" | wc -l')
+    # file_num =
+    os.system('find vending_accounts/ -name "*.p" | wc -l')
     print("Account List:")
-    #file_num=sp.check_output(['find','vending_accounts/','-name','"*.p"','|','wc','-l'])
-    os.system('find vending_accounts/ -name "*.p" |  grep -oP "(?<=vending_accounts/)\w+"')
+    # file_num=sp.check_output(['find','vending_accounts/','-name','"*.p"','|','wc','-l'])
+    os.system(
+        'find vending_accounts/ -name "*.p" |  grep -oP "(?<=vending_accounts/)\w+"')
     #file_list=sp.call("ls vending_accounts/*.p")
+
 
 def load_account(username=None):
     try:
         if username is None:
-            filename="vending_accounts/"+input("Please input the username: ")+".p"
+            filename = "vending_accounts/" + \
+                input("Please input the username: ") + ".p"
         else:
-            filename="vending_accounts/"+username+".p"
-        old_account=pickle.load(open(filename,"rb"))
-    except FileNotFoundError:
-        print("Username not found.")
-        old_account=None
+            filename = "vending_accounts/" + username + ".p"
+        old_account = pickle.load(open(filename, "rb"))
+    except OSError:
+        print("Username not found, or IO error.")
+        old_account = None
     return old_account
+
 
 def make_new_account(username=None):
     if username is None:
-        username=input("Username: ")
+        while True:
+            username = input("Username (letters and numbers only): ")
+            if re.match("^[A-Za-z0-9]*$", username):
+                break
     while True:
-        password=getpass.getpass("Password: ")
-        passwd2=getpass.getpass("Password again:")
-        if passwd2==password:
+        password = getpass.getpass("Password: ")
+        passwd2 = getpass.getpass("Password again:")
+        if passwd2 == password:
             break
-    print("Are you sure to create the new account "+username+"?")
+    print("Are you sure to create the new account " + username + "?")
     if yes_or_no():
-        new_account=account(username,password)
+        new_account = account(username, password)
         new_account.save()
+
 
 def main():
     while True:
-        
+
         print_account_list()
-        
+
         print("Please choose an action:")
         # print("1. List all accounts.")
         print("1. Create a new account.")
         print("2. View one account.")
         print("3. Edit one account.")
         print("4. Exit.")
-        action=int(input("Input (1-4): "))
-        
-        if action==1:
+        action = int(input("Input (1-4): "))
+
+        if action == 1:
             make_new_account()
-            
-        if action==2:
-            old_account=load_account()
+
+        if action == 2:
+            old_account = load_account()
             if old_account is not None:
                 # print(old_account)
                 old_account.print_data()
-            
-        if action==3:
-            old_account=load_account()
+
+        if action == 3:
+            old_account = load_account()
             if old_account is not None:
                 old_account.print_data()
                 old_account.edit_data()
-            
-                
-        if action==4:
+
+        if action == 4:
             break
-        
+
+
 if __name__ == "__main__":
     main()
--- fight.py
+++ fight.py
@@ -1,59 +1,129 @@
 from random import randint
+from time import sleep
 
-def print_damage(attacker="attacker_name",target="target_name",damage=0,HP_left=10):
-    line=["%s kicked %s, %s got hurted by %d HP (%d left).",
-          "%s sprayed fire on %s, %s was burned by %d HP (%d left).",
-          "%s zapped %s, %s took damage by %d HP (%d left).",
-          "%s bombed %s and %s took %d HP (%d left)."
-            ]
-    i=randint(0,len(line))
-    print(line[i]%(attacker,target,target,damage,HP_left))
 
-def print_skip(attacker="attacker_name",target="target_name"):
-    line=["%s tried to kick %s, %s dodged.",
-          "%s tried to spray fire on %s, %s doged.",
-          "%s tried to zapp %s, %s doged it.",
-          "%s tried to bomb %s, %s doged."
+class color:
+    default = "\033[39m"
+    black = " \033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    Yellow = "\033[33m"
+    blue = "\033[34m"
+    magenta = "\033[35m"
+    cyan = "\033[36m"
+
+
+def print_damage(attacker="attacker_name", target="target_name", damage=0, HP_left=10):
+    line = ["%s kicked %s, %s got hurted by %d HP (%d left).",
+            "%s sprayed fire on %s, %s was burned by %d HP (%d left).",
+            "%s zapped %s, %s took damage by %d HP (%d left).",
+            "%s bombed %s and %s took %d HP (%d left)."
             ]
-    i=randint(0,len(line))
-    print(line[i]%(attacker,target,target))
+    i = randint(0, len(line) - 1)
+    print(line[i] % (attacker, target, target, damage, HP_left))
+
+
+def print_skip(attacker="attacker_name", target="target_name"):
+    line = ["%s tried to kick %s, %s dodged.",
+            "%s tried to spray fire on %s, %s doged.",
+            "%s tried to zapp %s, %s doged it.",
+            "%s tried to bomb %s, %s doged."
+            ]
+    i = randint(0, len(line) - 1)
+    print(color.green + line[i] % (attacker, target, target) + color.default)
+
 
 def print_lose(loser_name="target"):
-    line=["%s was kicked out of the fighting ring.",
-          "%s was \"fired\" out of the arena",
-          "%s was eletrouted to the ground",
-          "%s ran away...",
-          "%s "
+    line = ["%s was kicked out of the fighting ring.",
+            "%s was \"fired\" out of the arena",
+            "%s was eletrouted to the ground",
+            "%s ran away...",
+            "%s took out a piece of white sheet, and waving~~~ waving~~~"
             ]
-    i=randint(0,len(line))
-    print(line[i]%(loser_name))
+    i = randint(0, len(line) - 1)
+    print("\n" + color.magenta + line[i] % (loser_name) + color.default)
 
-def fight_start(p1_name="P1",p2_name="P2",
-            p1_att=[125,125,125,125,125],
-            p2_att=[100,125,75,160,160]):
-    # player attrs: HP, attack, defense, speed, luck
-    # speed decides attack frequency, each player has an accumulator, which reach each 200, the player will attack
+
+def attack(attacker, target, attacker_att, target_att, target_HP):
     # speed also helps dodge. If player 1 has double speed of player 2, the % of dodge doubles.
-    # luck decides dodge and crtical hits. The default dodge and critical hit % is 20. 
+    # luck decides dodge and crtical hits. The default dodge and critical hit % is 20.
     # if one player has double luck the both % doubled.
-    # damage = attack * max(1,attack/defense), critical double attack.
-    # HP is inputed HP*5
-    HP1=p1_att[0]*5
-    HP2=p2_att[0]*5
-    accumulator1=0
-    accumulator2=0
+    # damage = attack * min(1,attack/defense), critical double attack, the initial attack tuned by luck .
+
+    dodge_rate = min(
+        0.9, 0.2 * target_att[3] / attacker_att[3] * target_att[4] / attacker_att[4])
+    critical_rate = min(0.7, 0.2 * attacker_att[4] / target_att[4])
+    dodge = (randint(0, 100) < 100 * dodge_rate)
+    critical = (randint(0, 100) < 100 * critical_rate)
+    if dodge:
+        print_skip(attacker, target)
+        return target_HP
+    else:
+        att = attacker_att[1] * (1 + randint(0, round(70 *
+                                                      min(1, attacker_att[4] / target_att[4]))) / 100)
+        if critical:
+            att = att * 2
+            print(color.red + "Critical HIT: ", end="")
+
+        damage = att * min(1, attacker_att[1] / target_att[2])
+        target_HP = max(0, target_HP - damage)
+        print_damage(attacker, target, damage, target_HP)
+        print(color.default, end="")
+        return target_HP
+
+
+def fight_start(p1_name="P1", p2_name="P2",
+                p1_att=[425, 225, 125, 55, 55],
+                p2_att=[100, 25, 55, 460, 160], pause_time=0.5):
+    # player attrs: HP 0, attack 1, defense 2, speed 3, luck 4
+    # speed decides attack frequency, each player has an accumulator, which reach each 200, the player will attack
+    # HP is inputed HP*50
+
+    HP1 = p1_att[0] * 5
+    HP2 = p2_att[0] * 5
+    accumulator1 = 0
+    accumulator2 = 0
     while True:
+        sleep(pause_time)
         accumulator1 += p1_att[3]
         accumulator2 += p2_att[3]
-        if accumulator1>accumulator2:
-            if accumulator1
-        
-    
+        if accumulator1 > accumulator2:
+            while accumulator1 >= 200 or accumulator2 >= 200:
+                if accumulator1 >= 200:
+                    accumulator1 -= 200
+                    HP2 = attack(p1_name, p2_name, p1_att, p2_att, HP2)
+                    if HP2 <= 0:
+                        print_lose(p2_name)
+                        return 1  # return winner number.
+                if accumulator2 >= 200:
+                    accumulator2 -= 200
+                    HP1 = attack(p2_name, p1_name, p2_att, p1_att, HP1)
+                    if HP1 <= 0:
+                        print_lose(p1_name)
+                        return 2  # return winner number.
+        else:
+            while accumulator2 >= 200 or accumulator1 >= 200:
+                if accumulator2 >= 200:
+                    accumulator2 -= 200
+                    HP1 = attack(p2_name, p1_name, p2_att, p1_att, HP1)
+                    if HP1 <= 0:
+                        print_lose(p1_name)
+                        return 2  # return winner number.
+                if accumulator1 >= 200:
+                    accumulator1 -= 200
+                    HP2 = attack(p1_name, p2_name, p1_att, p2_att, HP2)
+                    if HP2 <= 0:
+                        print_lose(p2_name)
+                        return 1  # return winner number.
+
     # hello i play more minigame bye  |    The fight is a mini game   |   damage= 'damage  |   I go ZZZZZZZZZ   |'
     # I have water in my mouth so cant talk   |     water because my teeth hurt   |     ?????!!!!!!!
- 
+
+
 def main():
-    print_damage("P1","P2",3,10)
- 
+    # print_damage("P1", "P2", 3, 10)
+    fight_start(pause_time=0)
+
+
 if __name__ == "__main__":
     main()
